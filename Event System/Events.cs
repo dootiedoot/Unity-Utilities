580314c87dae0af28553dd619b19795f0da8001c
Chad Hoang <choang05@gmail.com>
2022-05-02 23:10:37 -0500

    Added Event System to Utilities. (Need to add documentation version)

---
+++ Event System/Events.cs
@@ -0,0 +1,5 @@
+public static class Events
+{
+    //  [TITLE/CATAGORY]
+    public const string EVENT_NAME_EXAMPLE = nameof(EVENT_NAME_EXAMPLE);  //  (string)NAME_OF_PARAMETER, (int)NAME_OF_PARAMETER2, (float)NAME_OF_PARAMETER3 ...
+}
